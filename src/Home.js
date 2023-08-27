756f6c9c01cdbc7816dc574d8a3dd8d97b97aa23
Sami Ribardiere <sxr089@student.bham.ac.uk>
2023-08-27 17:38:09 +0200

    first

---
+++ src/Home.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import BlogList from "./blogList";
+
+
+
+const Home = () => {
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
+
+    }
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ])
+    
+
+        
+    
+    return ( 
+        <div className="home">
+           <BlogList blogs = {blogs} title = "All Blogs" handleDelete={handleDelete}/>
+           
+        </div>
+     );
+
+    }
+
+ 
+export default Home;
+
