848f01e61d192eeb5f4420e2f2418459fce41f0d
Sami Ribardiere <sxr089@student.bham.ac.uk>
2023-08-28 10:15:54 +0200

    product2

--- src/navbar.js
+++ src/navbar.js
@@ -4,7 +4,7 @@ import { Dialog } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
 const navigation = [
-  { name: 'Product', href: '/product' },
+  { name: 'Product', href: '/#product' },
   { name: 'Features', href: '#' },
   { name: 'Marketplace', href: '#' },
   { name: 'Company', href: '#' },
