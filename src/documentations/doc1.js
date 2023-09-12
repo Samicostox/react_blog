6fb5dbfcf4d81f65f8e87d345bbc52e5488a81c5
Sami Ribardiere <sxr089@student.bham.ac.uk>
2023-09-12 10:31:39 +0200

    new image

--- src/documentations/doc1.js
+++ src/documentations/doc1.js
@@ -21,7 +21,7 @@ const lottieStyle = {
 };
 
 export default function Doc1() {
-  const imageSrc = "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply";
+  const imageSrc = "https://res.cloudinary.com/dl2adjye7/image/upload/v1694507175/subtle-prism_waud51.png";
   const text = "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.";
   const title = "Cold Outreach"
 
