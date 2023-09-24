b632f2b91c2f82bf82e08a2da000d9ed7a70c244
Sami <sxr089@student.bham.ac.uk>
2023-09-24 23:41:06 +0200

    particles

--- src/website/carousel.js
+++ src/website/carousel.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect,useRef  } from "react";
-import Particles from "react-tsparticles"; 
-import { loadFull } from "tsparticles"; 
+
 
 const ChevronRightIcon = () => <span>→</span>;
 
@@ -127,92 +126,14 @@ const Carousel = () => {
     resetTimer();
   };
 
-  {/*
-  const particlesInit = async (main) => {
-    console.log(main);
 
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(main);
-  };
-
-  */}
 
 
   return (
     
     <div className="relative overflow-hidden" >
-      {/*
-     <Particles
-  init={particlesInit}
-  id="tsparticles"
-  className="absolute inset-0 z-10"
-  options={{
-    background: {
-      color: "white",
-    },
-    fpsLimit: 60,
-    particles: {
-      number: {
-        value: 80,
-        density: {
-          enable: true,
-          value_area: 800,
-        },
-      },
-      color: {
-        value: "#008000", // Darker green color
-      },
-      shape: {
-        type: "circle",
-      },
-      opacity: {
-        value: 0.5,
-      },
-      size: {
-        value: 5,
-        random: true,
-      },
-      links: {
-        enable: true,
-        distance: 150,
-        color: "#008000",
-        opacity: 0.4,
-        width: 1,
-      },
-      move: {
-        enable: true,
-        speed: 2, // Adjust the speed as needed
-        direction: "none",
-        out_mode: "out",
-      },
-    },
-    interactivity: {
-      detectsOn: "canvas",
-      events: {
-        onHover: {
-          enable: true,
-          mode: "repulse",
-        },
-        onClick: {
-          enable: true,
-          mode: "push",
-        },
-        resize: true,
-      },
-      modes: {
-        repulse: {
-          distance: 100,
-        },
-      },
-    },
-    retina_detect: true,
-  }}
-/>
-
-*/}
-
+     
+     
 
 
 
