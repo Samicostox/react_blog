f5e1aa07d92e0a38954398a1899fbc0cef160cc2
HHazard2002 <hugo7.hazard@gmail.com>
2023-10-15 20:39:51 +0100

    ok

--- src/website/abous-us.js
+++ src/website/abous-us.js
@@ -146,114 +146,116 @@ export default function AboutUs() {
 
       <Content />
       <div className="mx-auto max-w-[1700px] px-6 lg:px-8 lg:pl-14 pt-10   mt-10">
-          <div style={{ height: "1px", backgroundColor: "black" }}></div>
+        <div style={{ height: "1px", backgroundColor: "black" }}></div>
 
-          <div className="mt-10">
+        <div className="mt-10">
           <InnovationStudios />
-          </div>
         </div>
+      </div>
 
+      <div className="mx-auto max-w-[1700px] px-6 lg:px-8 lg:pl-14 pt-10   mt-10">
+        <div style={{ height: "1px", backgroundColor: "black" }}></div>
 
-        <div className="mx-auto max-w-[1700px] px-6 lg:px-8 lg:pl-14 pt-10   mt-10">
-          <div style={{ height: "1px", backgroundColor: "black" }}></div>
-
-          <div className="mt-10">
-      <div className="relative pb-36 z-10 bg-white w-full ">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8  ">
-          <div className="w-full bg-white h-20 relative z-10">
-            {/* Content of the container goes here */}
-          </div>
-          <div className="mx-auto max-w-2xl lg:max-w-none">
-          <div className="text-center">
-                <h2 className="text-3xl font-alliance font-bold tracking-tight text-gray-900 sm:text-4xl ">
-                  Our Year In Results
-                </h2>
-            </div>
-            <dl className="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4">
-              {stats.map((stat) => (
-                <div key={stat.id} className="flex flex-col bg-teal-900 p-8">
-                  <dt className="text-sm font-semibold leading-6 text-white font-alliance">
-                    {stat.name}
-                  </dt>
-                  <dd className="order-first text-3xl font-semibold tracking-tight text-white font-alliance font-bold">
-                    {stat.value}
-                  </dd>
+        <div className="mt-10">
+          <div className="relative pb-36 z-10 bg-white w-full ">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8  ">
+              <div className="w-full bg-white h-20 relative z-10">
+                {/* Content of the container goes here */}
+              </div>
+              <div className="mx-auto max-w-2xl lg:max-w-none">
+                <div className="text-center">
+                  <h2 className="text-3xl font-alliance font-bold tracking-tight text-gray-900 sm:text-4xl ">
+                    Our Year In Results
+                  </h2>
                 </div>
-              ))}
-            </dl>
-          </div>
-        </div>
-      </div>
-      </div>
-        </div>
-        <div className="mx-auto max-w-[1700px] px-6 lg:px-8 lg:pl-14 pt-10   mt-10">
-          <div style={{ height: "1px", backgroundColor: "black" }}></div>
-
-          <div className="mt-20">
-      <Services />
-      </div>
-        </div>
-
-        <div className="mx-auto max-w-[1700px] px-6 lg:px-8 lg:pl-14 pt-10   mt-10">
-          <div style={{ height: "1px", backgroundColor: "black" }}></div>
-
-          <div className="mt-20">           
-      <h1 className="text-center text-4xl text-black  font-alliance font-bold ">
-        OUR APPROACH
-      </h1>
-      <p className="mt-6 text-lg leading-8 text-gray-600 text-center font-alliance mb-12 pb-10">
-        From inception to delivery we are here all along the way.
-      </p>
-      <NumberLine />
-
-      <div className="bg-white">
-        <div className="mx-auto  max-w-[1500px] py-24 sm:px-6 sm:py-32 lg:px-8">
-          <div className="relative isolate overflow-hidden bg-green-800 px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
-            <h2 className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl font-alliance">
-              Ready to dive in?
-            </h2>
-            <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300 font-alliance">
-              Let's start our journey together! Simply fill in our form and we
-              will be in touch.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="/#/contact2"
-                className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-green-900 shadow-sm hover:bg-green-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white font-alliance"
-              >
-                Contact Us
-              </a>
-              <a
-                href="/#/services"
-                className="text-sm font-semibold leading-6 text-white font-alliance"
-              >
-                Learn more <span aria-hidden="true">→</span>
-              </a>
+                <dl className="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4">
+                  {stats.map((stat) => (
+                    <div
+                      key={stat.id}
+                      className="flex flex-col bg-teal-900 p-8"
+                    >
+                      <dt className="text-sm font-semibold leading-6 text-white font-alliance">
+                        {stat.name}
+                      </dt>
+                      <dd className="order-first text-3xl font-semibold tracking-tight text-white font-alliance font-bold">
+                        {stat.value}
+                      </dd>
+                    </div>
+                  ))}
+                </dl>
+              </div>
             </div>
-            <svg
-              viewBox="0 0 1024 1024"
-              className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]"
-              aria-hidden="true"
-            >
-              <circle
-                cx={512}
-                cy={512}
-                r={512}
-                fill="url(#827591b1-ce8c-4110-b064-7cb85a0b1217)"
-                fillOpacity="0.7"
-              />
-              <defs>
-                <radialGradient id="827591b1-ce8c-4110-b064-7cb85a0b1217">
-                  <stop stopColor="#4CAF50" />
-                  <stop offset={1} stopColor="#81C784" />
-                </radialGradient>
-              </defs>
-            </svg>
           </div>
         </div>
       </div>
-      </div>
+      <div className="mx-auto max-w-[1700px] px-6 lg:px-8 lg:pl-14 pt-10   mt-10">
+        <div style={{ height: "1px", backgroundColor: "black" }}></div>
+
+        <div className="mt-20">
+          <Services />
         </div>
+      </div>
+
+      <div className="mx-auto max-w-[1700px] px-6 lg:px-8 lg:pl-14 pt-10   mt-10">
+        <div style={{ height: "1px", backgroundColor: "black" }}></div>
+
+        <div className="mt-20">
+          <h1 className="text-center text-4xl text-black  font-alliance font-bold ">
+            OUR APPROACH
+          </h1>
+          <p className="mt-6 text-lg leading-8 text-gray-600 text-center font-alliance mb-12 pb-10">
+            From inception to delivery we are here all along the way.
+          </p>
+          <NumberLine />
+
+          <div className="bg-white">
+            <div className="mx-auto  max-w-[1500px] py-24 sm:px-6 sm:py-32 lg:px-8">
+              <div className="relative isolate overflow-hidden bg-green-800 px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
+                <h2 className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl font-alliance">
+                  Ready to dive in?
+                </h2>
+                <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300 font-alliance">
+                  Let's start our journey together! Simply fill in our form and
+                  we will be in touch.
+                </p>
+                <div className="mt-10 flex items-center justify-center gap-x-6">
+                  <a
+                    href="/#/contact2"
+                    className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-green-900 shadow-sm hover:bg-green-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white font-alliance"
+                  >
+                    Contact Us
+                  </a>
+                  <a
+                    href="/#/services"
+                    className="text-sm font-semibold leading-6 text-white font-alliance"
+                  >
+                    Learn more <span aria-hidden="true">→</span>
+                  </a>
+                </div>
+                <svg
+                  viewBox="0 0 1024 1024"
+                  className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]"
+                  aria-hidden="true"
+                >
+                  <circle
+                    cx={512}
+                    cy={512}
+                    r={512}
+                    fill="url(#827591b1-ce8c-4110-b064-7cb85a0b1217)"
+                    fillOpacity="0.7"
+                  />
+                  <defs>
+                    <radialGradient id="827591b1-ce8c-4110-b064-7cb85a0b1217">
+                      <stop stopColor="#4CAF50" />
+                      <stop offset={1} stopColor="#81C784" />
+                    </radialGradient>
+                  </defs>
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <Footer />
     </div>
