dc977ea4cbb17df579c42c0466b0b410ef49fa09
uobjuniorsolutions <uonjuniorsolutions@gmail.com>
2024-03-02 12:17:15 +0000

    update under texts

--- src/new_website/Team/Components/benefits.js
+++ src/new_website/Team/Components/benefits.js
@@ -131,9 +131,25 @@ import 'aos/dist/aos.css';
       <div className="bg-zinc-50 px-2 md:px-6 ">
         <div className="mb-8 px-4 pt-40">
           <h3 className="text-slate-900 text-5xl font-semibold text-center text-left font-alliance">
-            Our <span className="relative inline-flex items-center justify-center text-slate-900 font-alliance font-bold">
-              {/* SVG and Benefits text remains unchanged */}
-            </span>
+           The {' '}
+            <span className="inline-flex items-center justify-center text-teal-700 relative z-10 font-alliance">
+      <svg
+        className="absolute left-0 top-full -mt-4 max-w-full"
+        width="220"
+        height="24"
+        viewBox="0 0 220 24"
+        xmlns="http://www.w3.org/2000/svg"
+        style={{zIndex: '-1'}} // Explicitly set for clarity, though it's implicitly under the text due to DOM structure
+      >
+        <path
+          d="M134.66 13.107c-10.334-.37-20.721-.5-31.12-.291l-2.6.06c-4.116.04-8.193.602-12.3.749-14.502.43-29.029 1.196-43.514 2.465-6.414.63-12.808 1.629-19.04 2.866-7.93 1.579-16.113 3.71-23.367 5.003-2.211.374-3.397-1.832-2.31-4.906.5-1.467 1.838-3.456 3.418-4.813a16.047 16.047 0 0 1 6.107-3.365c16.88-4.266 33.763-6.67 51.009-7.389C71.25 3.187 81.81 1.6 92.309.966c11.53-.65 23.097-.938 34.66-.96 7.117-.054 14.25.254 21.36.318l16.194.803 4.62.39c3.85.32 7.693.618 11.53.813 8.346.883 16.673.802 25.144 2.159 1.864.276 3.714.338 5.566.873l.717.225c6.162 1.977 7.92 3.64 7.9 7.197l-.003.203c-.017.875.05 1.772-.112 2.593-.581 2.762-4.066 4.12-8.637 3.63-13.696-1.06-27.935-3.332-42.97-4.168-11.055-.83-22.314-1.459-33.596-1.603l-.022-.332Z"
+          fill="#4fd1c5" // Lighter teal color
+          fillRule="evenodd"
+        />
+      </svg>
+      Benefits
+    </span> of working with us!
+                  
           </h3>
           <p className="text-center text-sm mt-2 max-w-lg mx-auto font-alliance text-slate-900">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus consequatur reprehenderit.
